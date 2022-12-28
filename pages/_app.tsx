2e6767504698dea18a62cbac9f1214d2a9ed0a1f
MudassirKz <98865127+Mudassirkz@users.noreply.github.com>
2022-12-28 17:17:35 +0500

    done some work.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleOAuthProvider clientId="300966724203-9e64cote67b4fosd50iis8jlpas6lp1h.apps.googleusercontent.com">
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  );
 }
