4dbcf73b4f3165ac069eec8b9d0b5a55c5caf27f
MudassirKz <98865127+Mudassirkz@users.noreply.github.com>
2023-01-11 17:51:39 +0500

    some more updates.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d491222711ec05cb"; }
+/******/ 		__webpack_require__.h = function() { return "2ceeb8e6e28ae609"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
